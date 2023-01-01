ae7421c8d67ce62e3d90be2c57f3a4f3a746dd94
kainesv86 <kainesv86@gmail.com>
2023-01-01 21:59:15 +0700

    Fix ui

--- src/components/Input/InputSlider.tsx
+++ src/components/Input/InputSlider.tsx
@@ -1,21 +1,47 @@
 import * as React from 'react';
+import { useFormContext } from 'react-hook-form';
 import SliderUnstyled, { SliderUnstyledProps } from '@mui/base/SliderUnstyled';
 
+import CommonFieldWrapper from './CommonFieldWrapper';
+
 interface InputSliderProps extends SliderUnstyledProps {
+	name: string;
 	label?: string;
+	isRequire?: boolean;
+	direction?: 'row' | 'column';
 }
 
 export const InputSlider = React.forwardRef(function Slider(
 	props: InputSliderProps,
 	ref: React.ForwardedRef<HTMLSpanElement>,
 ) {
+	const { setValue, register } = useFormContext();
+
+	const [input, setInput] = React.useState<number | number[]>(0);
+
+	React.useEffect(() => {
+		register(props.name);
+	}, []);
+
+	React.useEffect(() => {
+		setValue(props.name, input);
+	}, [input]);
+
 	return (
-		<div className="flex flex-col gap-2">
-			{props.label && <label className="text-sm font-medium">{props.label}</label>}
+		<CommonFieldWrapper
+			name={props.name}
+			label={props.label}
+			isRequire={props.isRequire}
+			direction={props.direction}
+		>
 			<SliderUnstyled
 				{...props}
+				onChange={(_, newValue) => {
+					setInput(newValue);
+				}}
 				ref={ref}
 				slotProps={{
+					valueLabel: { className: 'absolute text-sm -bottom-5 font-medium' },
 					thumb: {
 						className:
 							'ring-cyan-500 dark:ring-cyan-400 ring-2 w-4 h-4 -mt-1 -ml-2 flex items-center justify-center bg-white rounded-full shadow absolute',
@@ -27,6 +53,6 @@ export const InputSlider = React.forwardRef(function Slider(
 					track: { className: 'bg-cyan-500 dark:bg-cyan-400 h-2 absolute rounded-full' },
 				}}
 			/>
-		</div>
+		</CommonFieldWrapper>
 	);
 });
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,12 +1,41 @@
 import * as React from 'react';
-import { InputSlider } from 'src/components/Input';
+import { useForm } from 'react-hook-form';
+
+import { FormWrapper, InputSlider } from 'src/components/Input';
 
 interface TestPageProps {}
 
 const TestPage: React.FunctionComponent<TestPageProps> = () => {
+	const methods = useForm();
+
+	const handleOnSubmit = async (data: any) => {
+		console.log(data);
+	};
+
 	return (
-		<div className="flex flex-col w-full h-full min-h-screen p-4 bg-red-400">
-			<InputSlider label="Heat" />
+		<div className="flex flex-col w-full h-full min-h-screen p-10 bg-indigo-200">
+			<div className="max-w-2xl px-10 py-20 bg-white rounded">
+				<FormWrapper methods={methods}>
+					<form onSubmit={methods.handleSubmit(handleOnSubmit)} className="space-y-5">
+						<InputSlider name="Heat" label="Heat" defaultValue={[1]} valueLabelDisplay={'auto'} />
+						<InputSlider
+							name="Price"
+							label='Price (from "$" to "$$$")'
+							defaultValue={[10000, 100000000]}
+							step={10000}
+							max={100000000}
+							min={10000}
+							valueLabelDisplay={'auto'}
+						/>
+						<button
+							type="submit"
+							className="flex justify-center px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3 bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-600 hover:to-blue-700"
+						>
+							Submit
+						</button>
+					</form>
+				</FormWrapper>
+			</div>
 		</div>
 	);
 };
